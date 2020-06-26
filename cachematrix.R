cfd5c9bc59c4f591530801d1f9bd03f57f907583
NubiaforData <64439734+NubiaforData@users.noreply.github.com>
2020-06-26 11:25:50 -0600

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Set the input x equal the matrix
+## set the function y to NULL
 
-## Write a short comment describing this function
+## I change the reference to solve 
 
 makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() (x)
+  setInverse <- function(inverse) (inv <<- inverse)
+  getInverse <- function () (inv)
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 
 }
 
 
-## Write a short comment describing this function
+## I change the reference to solve
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if(!is.null(inv)){
+    message("getting cached data")
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setInverse(inv)
+  inv 
 }
+## Return a matrix that is the inverse of 'x'
